e1a2ca111c27844ef27555d75e116be4f0d7ce03
withgosec <125962366+withgosec@users.noreply.github.com>
2023-02-21 09:57:32 +0100

    Update scirpt.js

--- scirpt.js
+++ scirpt.js
@@ -1,1 +1,27 @@
-<html>test</html>
+function checkAll(className) {
+    let radios = document.getElementsByClassName(className)
+    Array.from(radios).map(x => x.checked = "true")
+}
+
+function uncheckAll() {
+    let radios = document.getElementsByTagName("input")
+    Array.from(radios).map(x => x.checked = "")
+}
+
+function openLogin() {
+    document.getElementById("login-popup").style.display = "block"
+    document.getElementById("open-login-button").style.display = "none"
+    document.getElementById("login-info").style.display = "none"
+}
+
+function closeLogin() {
+    document.getElementById("login-popup").style.display = "none"
+    document.getElementById("open-login-button").style.display = "block"
+    document.getElementById("login-info").style.display = "none"
+}
+
+function submitLogin() {
+    // make a POST rewuest with login details
+    let email = document.getElementById("email").value.trim();
+    let password = document.getElementById("password").value.trim();
+}
